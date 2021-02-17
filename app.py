c895564ba676bfa65fe979ae5979769fc3f68bab
Aditya <38887904+adi0808@users.noreply.github.com>
2021-02-17 22:38:27 +0530

    updating app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api
+from Resources.fetch import Fetch
+from Resources.pay import Pay
+
+app = Flask(__name__)
+api = Api(app)
+
+
+# Resources end point
+api.add_resource(Fetch, "/setu/fetch")
+api.add_resource(Pay, "/setu/pay")
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
